91589f8f9a737aee03c8fdad2f811e3ef816c329
Simas Leonavicius <simas.leonavicius@devbridge.com>
2023-02-26 20:40:13 +0200

    Add new failing tests for Homepage. And some content.

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -3,13 +3,45 @@ import Home from './page';
 import '@testing-library/jest-dom';
 
 describe('page', () => {
-    it('Renders a title', () => {
+    it('Should renders a title', () => {
         render(<Home />);
 
         const heading = screen.getByRole('heading', {
-            name: 'Home page',
+            name: 'Welcome to our Brewery!',
         });
 
         expect(heading).toBeInTheDocument();
+    });
+
+    it('Should render a sub-title', () => {
+        render(<Home />);
+
+        const subTitle = screen.getByTestId('subTitle');
+
+        expect(subTitle).toBeInTheDocument();
+    });
+
+    it('Should render a primary CTA', () => {
+        render(<Home />);
+
+        const primaryAction = screen.getByTestId('primaryAction');
+
+        expect(primaryAction).toBeInTheDocument();
+    });
+
+    it('Should render a secondary CTA', () => {
+        render(<Home />);
+
+        const secondaryAction = screen.getByTestId('secondaryAction');
+
+        expect(secondaryAction).toBeInTheDocument();
+    });
+
+    it('Should render an accent image', () => {
+        render(<Home />);
+
+        const accentImage = screen.getByTestId('accentImage');
+
+        expect(accentImage).toBeInTheDocument();
     });
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,11 @@ import styles from './page.module.scss';
 export default function Home() {
     return (
         <div>
-            <h1 className={styles.title}>Home page</h1>
-            <p>Some content</p>
+            <h1 className={styles.title}>Welcome to our Brewery!</h1>
+            <span data-testid="subTitle">
+                Finest drinks since King Arthur. Don&apos;t believe us? Come and
+                try it!
+            </span>
         </div>
     );
 }
